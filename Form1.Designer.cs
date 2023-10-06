058a796e9533d2f70544a2c8db1fd28bcdf0a7dd
luis <alumno@PC-LABA-25>
2023-10-06 13:08:00 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SageKiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            ButtnImage = new Button();
+            SuspendLayout();
+            // 
+            // ButtnImage
+            // 
+            ButtnImage.BackgroundImage = (Image)resources.GetObject("ButtnImage.BackgroundImage");
+            ButtnImage.BackgroundImageLayout = ImageLayout.Stretch;
+            ButtnImage.Location = new Point(335, 128);
+            ButtnImage.Name = "ButtnImage";
+            ButtnImage.Size = new Size(103, 117);
+            ButtnImage.TabIndex = 0;
+            ButtnImage.UseVisualStyleBackColor = true;
+            ButtnImage.Click += ButtnImage_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ButtnImage);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button ButtnImage;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,28 @@
+namespace SageKiller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void ButtnImage_Click(object sender, EventArgs e)
+        {
+            Random rdnx = new Random();
+            Random rdny= new Random();  
+            int x= rdnx.Next(0,713);
+            int y = rdny.Next(117, 332);
+
+
+            
+
+        }
+    }
+}
